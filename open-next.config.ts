ec470587069b359e9ab5a8e470ef2e251be2ea3d
Felipe Arce <felipearce.2004@gmail.com>
2026-08-31 17:17:54 -0400

    fix(infra): parchar loadCustomCacheHandlers y desactivar ofuscacion agresiva en OpenNext

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,9 +2,5 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 const config = defineCloudflareConfig();
 
-config.default = {
-  ...config.default,
-  minify: true,
-};
-
 export default config;
+
